ab8fa5b1da8cd0f5fcb72cefcdb3d7d04c4b18af
Artemis Stefanidou <71610745+ArtemisStefanidou@users.noreply.github.com>
2024-04-03 21:09:03 +0300

    Update main.py

--- CoperDBAPI/aisDecoder/main.py
+++ CoperDBAPI/aisDecoder/main.py
@@ -39,8 +39,9 @@ min_lon = 22.806318
 max_lon = 24.643915
 
 decoded_s = decode("!AIVDM,1,1,,A,137GAh001RR6v3TDS4HDdkb40D4h,0*35")
-decoded_s = decoded_s['msg_typ']
-logging.info(f'decoded_s: {decoded_s}')
+as_dict = decoded_s.asdict()
+# decoded_s = decoded_s['msg_typ']
+logging.info(f'decoded_s: {as_dict}')
 
 # while True:
 #     try:
