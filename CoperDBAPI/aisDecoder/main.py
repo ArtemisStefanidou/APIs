af64056dd9eaed0f45399e79db975b02589faffb
Artemis Stefanidou <71610745+ArtemisStefanidou@users.noreply.github.com>
2024-04-04 10:58:59 +0300

    Update main.py

--- CoperDBAPI/aisDecoder/main.py
+++ CoperDBAPI/aisDecoder/main.py
@@ -26,9 +26,9 @@ mycol_static = db["ais_cyprus_static"]
 athens_ais = db['athens_ais']
 all_ais = db['all_ais']
 
-# kafka_client = KafkaClient(hosts='kafka1:29092')
-# kafka_producer_dynamic = kafka_client.topics[b'ais_cyprus_dynamic'].get_producer()
-# kafka_producer_static = kafka_client.topics[b'ais_cyprus_static'].get_producer()
+kafka_client = KafkaClient(hosts='kafka1:29092')
+kafka_producer_dynamic = kafka_client.topics[b'ais_cyprus_dynamic'].get_producer()
+kafka_producer_static = kafka_client.topics[b'ais_cyprus_static'].get_producer()
 
 host = "0.0.0.0"
 port = 9094
@@ -56,7 +56,7 @@ while True:
             # decoded_b = decode(msg)
             message = decoded_b.asdict()
             ais = message
-            # logging.info(f'as_dict: {message}')
+            logging.info(f'as_dict: {message}')
             # message = msg.decode()
             # logging.info(f'message: {decoded_b}')
             
@@ -87,9 +87,9 @@ while True:
                     # logging.info(f'new_data: {new_data}')
                     mycol_dynamic.insert_one(new_data)
 
-                    # message_json = json.dumps(message_decoded)
-                    # message_bytes = message_json.encode('utf-8')
-                    # kafka_producer_dynamic.produce(message_bytes)
+                    message_json = json.dumps(message)
+                    message_bytes = message_json.encode('utf-8')
+                    kafka_producer_dynamic.produce(message_bytes)
 
                     if min_lat <= new_data["latitude"] <= max_lat and min_lon <= new_data["longitude"] <= max_lon:
                         db.athens_ais.insert_one(new_data)
@@ -107,15 +107,15 @@ while True:
     
                     db.ais_cyprus_dynamic.insert_one(new_data)
 
-                    # message_json = json.dumps(message_decoded)
-                    # message_bytes = message_json.encode('utf-8')
-                    # kafka_producer_dynamic.produce(message_bytes)
+                    message_json = json.dumps(message_decoded)
+                    message_bytes = message_json.encode('utf-8')
+                    kafka_producer_dynamic.produce(message_bytes)
                     
                     if min_lat <= new_data["latitude"] <= max_lat and min_lon <= new_data["longitude"] <= max_lon:
                         db.athens_ais.insert_one(new_data)
                         
                 elif message_type == 5:
-                    logging.info(f'as_dict: {message}')
+                    # logging.info(f'as_dict: {message}')
                     new_data["mmsi"] = message["mmsi"]
                     new_data["imo"] = message["imo"]
                     new_data["ship_name"] = message["shipname"]
@@ -131,13 +131,13 @@ while True:
 
                     db.ais_cyprus_static.insert_one(new_data)
 
-                    # message_json = json.dumps(message_decoded)
-                    # message_bytes = message_json.encode('utf-8')
-                    # kafka_producer_static.produce(message_bytes)
+                    message_json = json.dumps(message_decoded)
+                    message_bytes = message_json.encode('utf-8')
+                    kafka_producer_static.produce(message_bytes)
 
                 elif message_type == 24:
 
-                    logging.info(f'message: {message}')
+                    # logging.info(f'message: {message}')
 
                     new_data["mmsi"] = message["mmsi"]
                     new_data["imo"] = None
@@ -154,9 +154,9 @@ while True:
                     
                     db.ais_cyprus_static.insert_one(new_data)
 
-                    # message_json = json.dumps(message_decoded)
-                    # message_bytes = message_json.encode('utf-8')
-                    # kafka_producer_static.produce(message_bytes)
+                    message_json = json.dumps(message_decoded)
+                    message_bytes = message_json.encode('utf-8')
+                    kafka_producer_static.produce(message_bytes)
                     
                
                     
